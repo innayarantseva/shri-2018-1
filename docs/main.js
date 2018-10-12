34f5a6a4539e5f6a09a1ac7583a2b4c17662ac66
Inna Yarantseva <innayarantseva@gmail.com>
2018-10-13 01:46:03 +0300

    updated docs

--- docs/main.js
+++ docs/main.js
@@ -138,8 +138,7 @@ if ('content' in document.createElement('template')) {
                 cardData.innerHTML = '<img src="./assets/Richdata.svg" />';
             }
             if ( data.events[i].data.image ) {
-                cardData.innerHTML = `<img src="${ data.events[i].data.image }" />`;
-                cardData.innerHTML = `<div class="card__data-image" style="background-image: url('${ data.events[i].data.image }')"> <div/>`;
+                cardData.innerHTML = `<div class="card__data-image" touch-action=“none” style="background-image: url('${ data.events[i].data.image }')"> <div/>`;
             }
             if ( data.events[i].data.buttons ) {
                 const buttonsHTML = data.events[i].data.buttons.map(
@@ -187,12 +186,7 @@ if ('content' in document.createElement('template')) {
 
         if ( (data.events[i].source === 'Сенсор движения') && ('ontouchstart' in document.documentElement) ) {
             cardData.appendChild( document.importNode( controlTemplate.content, true ) );
-
-            /*cardData.addEventListener('pointerdown', (event) => {
-                console.log(event.type, event);
-            });*/
-
-
+            cardData.setAttribute( 'touch-action', 'none' );
 
             let currentGesture = null;
             const nodeState = { startPosition: 0 };
@@ -228,6 +222,7 @@ if ('content' in document.createElement('template')) {
 
             cardData.addEventListener('pointerup',     (e) => console.log(e.type));
             cardData.addEventListener('pointercancel', (e) => console.log(e.type));
+            cardData.addEventListener('pointerup',     (e) => console.log(e.type));
 
 
             console.log('touch');
@@ -258,7 +253,7 @@ if ('content' in document.createElement('template')) {
 /* 3 */
 /***/ (function(module) {
 
-module.exports = {"events":[{"type":"info","title":"Еженедельный отчет по расходам ресурсов","source":"Сенсоры потребления","time":"19:00, Сегодня","description":"Так держать! За последнюю неделю вы потратили на 10% меньше ресурсов, чем неделей ранее.","icon":"stats","data":{"type":"graph","values":[{"electricity":[["1536883200",115],["1536969600",117],["1537056000",117.2],["1537142400",118],["1537228800",120],["1537315200",123],["1537401600",129]]},{"water":[["1536883200",40],["1536969600",40.2],["1537056000",40.5],["1537142400",41],["1537228800",41.4],["1537315200",41.9],["1537401600",42.6]]},{"gas":[["1536883200",13],["1536969600",13.2],["1537056000",13.5],["1537142400",13.7],["1537228800",14],["1537315200",14.2],["1537401600",14.5]]}]},"size":"l"},{"type":"info","title":"Дверь открыта","source":"Сенсор входной двери","time":"18:50, Сегодня","description":null,"icon":"key","size":"s"},{"type":"info","title":"Уборка закончена","source":"Пылесос","time":"18:45, Сегодня","description":null,"icon":"robot-cleaner","size":"s"},{"type":"info","title":"Новый пользователь","source":"Роутер","time":"18:45, Сегодня","description":null,"icon":"router","size":"s"},{"type":"info","title":"Изменен климатический режим","source":"Сенсор микроклимата","time":"18:30, Сегодня","description":"Установлен климатический режим «Фиджи»","icon":"thermal","size":"m","data":{"temperature":24,"humidity":80}},{"type":"critical","title":"Невозможно включить кондиционер","source":"Кондиционер","time":"18:21, Сегодня","description":"В комнате открыто окно, закройте его и повторите попытку","icon":"ac","size":"m"},{"type":"info","title":"Музыка включена","source":"Яндекс.Станция","time":"18:16, Сегодня","description":"Сейчас проигрывается:","icon":"music","size":"m","data":{"albumcover":"https://avatars.yandex.net/get-music-content/193823/1820a43e.a.5517056-1/m1000x1000","artist":"Florence & The Machine","track":{"name":"Big God","length":"4:31"},"volume":80}},{"type":"info","title":"Заканчивается молоко","source":"Холодильник","time":"17:23, Сегодня","description":"Кажется, в холодильнике заканчивается молоко. Вы хотите добавить его в список покупок?","icon":"fridge","size":"m","data":{"buttons":["Да","Нет"]}},{"type":"info","title":"Зарядка завершена","source":"Оконный сенсор","time":"16:22, Сегодня","description":"Ура! Устройство «Оконный сенсор» снова в строю!","icon":"battery","size":"s"},{"type":"critical","title":"Пылесос застрял","source":"Сенсор движения","time":"16:17, Сегодня","description":"Робопылесос не смог сменить свое местоположение в течение последних 3 минут. Похоже, ему нужна помощь.","icon":"cam","data":{"image":"./assets/cleaner.jpg"},"size":"l"},{"type":"info","title":"Вода вскипела","source":"Чайник","time":"16:20, Сегодня","description":null,"icon":"kettle","size":"s"}]};
+module.exports = {"events":[{"type":"info","title":"Еженедельный отчет по расходам ресурсов","source":"Сенсоры потребления","time":"19:00, Сегодня","description":"Так держать! За последнюю неделю вы потратили на 10% меньше ресурсов, чем неделей ранее.","icon":"stats","data":{"type":"graph","values":[{"electricity":[["1536883200",115],["1536969600",117],["1537056000",117.2],["1537142400",118],["1537228800",120],["1537315200",123],["1537401600",129]]},{"water":[["1536883200",40],["1536969600",40.2],["1537056000",40.5],["1537142400",41],["1537228800",41.4],["1537315200",41.9],["1537401600",42.6]]},{"gas":[["1536883200",13],["1536969600",13.2],["1537056000",13.5],["1537142400",13.7],["1537228800",14],["1537315200",14.2],["1537401600",14.5]]}]},"size":"l"},{"type":"info","title":"Дверь открыта","source":"Сенсор входной двери","time":"18:50, Сегодня","description":null,"icon":"key","size":"s"},{"type":"info","title":"Уборка закончена","source":"Пылесос","time":"18:45, Сегодня","description":null,"icon":"robot-cleaner","size":"s"},{"type":"info","title":"Новый пользователь","source":"Роутер","time":"18:45, Сегодня","description":null,"icon":"router","size":"s"},{"type":"info","title":"Изменен климатический режим","source":"Сенсор микроклимата","time":"18:30, Сегодня","description":"Установлен климатический режим «Фиджи»","icon":"thermal","size":"m","data":{"temperature":24,"humidity":80}},{"type":"critical","title":"Невозможно включить кондиционер","source":"Кондиционер","time":"18:21, Сегодня","description":"В комнате открыто окно, закройте его и повторите попытку","icon":"ac","size":"m"},{"type":"info","title":"Музыка включена","source":"Яндекс.Станция","time":"18:16, Сегодня","description":"Сейчас проигрывается:","icon":"music","size":"m","data":{"albumcover":"https://avatars.yandex.net/get-music-content/193823/1820a43e.a.5517056-1/m1000x1000","artist":"Florence & The Machine","track":{"name":"Big God","length":"4:31"},"volume":80}},{"type":"info","title":"Заканчивается молоко","source":"Холодильник","time":"17:23, Сегодня","description":"Кажется, в холодильнике заканчивается молоко. Вы хотите добавить его в список покупок?","icon":"fridge","size":"m","data":{"buttons":["Да","Нет"]}},{"type":"info","title":"Зарядка завершена","source":"Оконный сенсор","time":"16:22, Сегодня","description":"Ура! Устройство «Оконный сенсор» снова в строю!","icon":"battery","size":"s"},{"type":"critical","title":"Пылесос застрял","source":"Сенсор движения","time":"16:17, Сегодня","description":"Робопылесос не смог сменить свое местоположение в течение последних 3 минут. Похоже, ему нужна помощь.","icon":"cam","data":{"image":"./assets/bitmap.png"},"size":"l"},{"type":"info","title":"Вода вскипела","source":"Чайник","time":"16:20, Сегодня","description":null,"icon":"kettle","size":"s"}]};
 
 /***/ })
 /******/ ]);
